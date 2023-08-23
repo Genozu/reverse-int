e49f67cd367192c7136e590fdc6c2f414f8cafa2
Nikita Pankov <genozu@yandex.by>
2023-08-23 20:40:12 +0300

    fix: add function in index file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    const str = `${n}`;
+    let result = '';
+    let i = 0;
+
+    while (i < str.length) {
+        result = result + str[str.length - (1 + i)];
+        i += 1;
+    }
+    return result;
 }
