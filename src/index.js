be811fb6f164a33ec1f310dd031d646fbceab588
Nikita Pankov <genozu@yandex.by>
2023-08-23 20:57:48 +0300

    fix: fix function reverse in index file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 module.exports = function reverse (n) {
-    const str = `${n}`;
+    let str = `${n}`;
     let result = '';
     let i = 0;
-
-    while (i < str.length) {
-        result = result + str[str.length - (1 + i)];
-        i += 1;
+  
+    if (str[0] === '-') {
+      str = str.slice(1);
     }
+    
+    while (i < str.length) {
+      result = result + str[str.length - (1 + i)];
+      i += 1;
+    }
+  
     return result;
 }
